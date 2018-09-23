a1a1dfc6b5e9a8b7e76d7e94a3a3e345554058f5
Opeoluwa Iyi-Kuyoro <opeoluwa.iyikuyoro@gmail.com>
2018-09-23 19:12:02 +0100

    feat(sw): setup service worker
    - add the service worker js file
    - ass the service worker script to index.html

---
+++ sw.js
@@ -0,0 +1,32 @@
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open('restaurant').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/restaurant.html?id=1',
+        '/restaurant.html?id=2',
+        '/restaurant.html?id=3',
+        '/restaurant.html?id=4',
+        '/restaurant.html?id=5',
+        '/restaurant.html?id=6',
+        '/restaurant.html?id=7',
+        '/restaurant.html?id=8',
+        '/restaurant.html?id=9',
+        '/restaurant.html?id=10',
+        '/data/restaurants.json',
+        '/css/styles.css',
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  console.log(event.request.url);
+
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
